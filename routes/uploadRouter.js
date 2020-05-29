e34cf52422042f9e95431afbe15c8cc8c0b01060
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2020-05-29 13:15:42 +0530

    upload router updated

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
 });
 
 const imageFileFilter = (req, file, cb) => {
-    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    if(!file.originalname.match(/\.(jpg|jpeg|png|JPG|JPEG)$/)) {
         return cb(new Error('You can upload only image files!'), false);
     }
     cb(null, true);
@@ -137,16 +137,16 @@ uploadRouter.route('/profilePicture/:id')
     res.statusCode = 403;
     res.end('GET operation not supported on /imageUpload');
 })
-.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, profilePictureUpload.single('imageFile'), (req, res) => {
+.post(cors.corsWithOptions, authenticate.verifyUser, profilePictureUpload.single('imageFile'), (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.json(req.file);
 })
-.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /imageUpload');
 })
-.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('DELETE operation not supported on /imageUpload');
 });
